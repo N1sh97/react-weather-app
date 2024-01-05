d257c66f50d048df28bdd3addc33adf3536f9f0d
N1sh97 <112947529+N1sh97@users.noreply.github.com>
2024-01-05 16:23:33 +0000

    updated the data in the return with the API call

--- src/Weather.js
+++ src/Weather.js
@@ -1,44 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
-import axios from "axios"
+import axios from "axios";
 
 export default function Weather() {
-  const apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
-  let city = "London";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  return (
-    <div className="Weather">
-      <form>
-        <input type="search" placeholder="Search for a city..."></input>
-        <input
-          type="submit"
-          value="Search"
-          class="tn-secondary btn-sm"
-          autoFocus="on"
-        ></input>
-      </form>
-      <h1>London</h1>
-      <ul>
-        <li>Monday 7:00</li>
-        <li>Mostly cloudy</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-            alt="weather icon"
-          />
-          <span className="temperature">6</span>
-          <span className="unit">°C</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Percipitation:15%</li>
-            <li>Humidity:72%</li>
-            <li> Wind:13 Km/h</li>
-          </ul>
+  const [weatherData, SetWeatherData] = useState({ ready: false });
+  function handleResponce(response) {
+    console.log(response.data);
+    SetWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/rain_light.png",
+      date: "Monday 08:00",
+      wind: response.data.main.wind.speed,
+      city: response.data.name,
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <input type="search" placeholder="Search for a city..."></input>
+          <input
+            type="submit"
+            value="Search"
+            class="tn-secondary btn-sm"
+            autoFocus="on"
+          ></input>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <img src={weatherData.iconUrl} alt={weatherData.description} />
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="unit">°C</span>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity:{weatherData.humidity}%</li>
+              <li> Wind:{weatherData.wind}Km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
+    let city = "London";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponce);
+
+    return "Loading...";
+  }
 }
