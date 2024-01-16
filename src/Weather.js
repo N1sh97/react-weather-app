3a4c2e9bf3a8cd8178237274b7ab6d2b15d70ab4
N1sh97 <112947529+N1sh97@users.noreply.github.com>
2024-01-16 17:54:50 +0000

    created a new component called weatherinfo.js to move the weather data from in between the form data

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./Weatherinfo";
 import axios from "axios";
 
 export default function Weather() {
